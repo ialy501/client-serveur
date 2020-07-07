147ac7da7bfa19405f74078c766f3a7cc8efcd65
ialy501 <razafiarisonialy@gmail.com>
2020-07-07 10:40:20 +0200

    correction

--- client.c
+++ client.c
@@ -1,44 +1,42 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
-#include<ctype.h>
-                
-void error(const char *msg)    
-                
-int main(int argc, char *argv[])
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include<sys/types.h>
+int main()
 {
-    int sockfd, port, n;            
-    struct sockaddr_in serv_addr;  
-    struct hostent *server;      
+
+    pid_t pid;
+    int id;
+    char msg[255];//variable qui contiendrat les messages
     char buffer[512];
-    if (argc < 3)                    
+    struct sockaddr_in informations;  //structure donnant les informations sur le serveur
+
+    /*initialisation du protocole, TCP  l'adresse de connection 127.0.0.1 (en local) et du port du serveur (1400)*/
+    informations.sin_family = AF_INET;
+    informations.sin_port = htons(2500);
+    informations.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    int socketID = socket(AF_INET, SOCK_STREAM, 0); // creation du socket propre au client
+
+    if (socketID == -1)    //test de création du socket
     {
-       fprintf(stderr,"usage %s nom du port\n", argv[0]);
-       exit(0);
+        perror("socket");
+        exit (-1);
     }
-    port = atoi(argv[2]);                     
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);  
-    if (sockfd < 0) 
-        error("Erreur d'ouverture de socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"Erreurn hote introuvable\n");
-        exit(0);
+
+    if ((connect(socketID, (struct sockaddr *) &informations, sizeof(struct sockaddr_in))) == -1)   //connexion au serveur
+    {
+        perror("connect");
+        exit (-1);
     }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;  
-    bcopy((char *)server->h_addr, 
-        (char *)&serv_addr.sin_addr.s_addr,
-        server->h_length);      
-    serv_addr.sin_port = htons(port);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
-        error("ERrreur de connection");   
-        bzero(buffer,512); 
+
+
+    memset(msg, 0, 255);
+    recv(socketID, msg, 255, 0);
+    printf ("%s\n", msg);
     FILE *f;    
     int mot = 0;
     char c;
@@ -49,22 +47,20 @@ int main(int argc, char *argv[])
         if(isspace(c)||c=='\t')
         mot++;    
     }             
-    write(sockfd, &words, sizeof(int));
+    write(socketID, &mot, sizeof(int));
        rewind(f);
     char ch ;
     while(ch != EOF)
     {
         fscanf(f , "%s" , buffer);
-        write(sockfd,buffer,512);
+        write(socketID,buffer,512);
         ch = fgetc(f);
     }    
-    printf("Le fichier est envoyer avec succès\n") 
-    close(sockfd);
-    return 0;       
-}
+    printf("Le fichier est envoyer avec succès\n");
+    
 
-void error(const char *msg)          
-{
-    perror(msg);
-    exit(0);
+    shutdown(socketID, SHUT_RDWR);// fermeture du socket
+
+    return 0;
+
 }
--- serveur.c
+++ serveur.c
@@ -1,61 +1,100 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h> 
+#include<signal.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <ctype.h>
-                
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char * argv[])           
+int main()
 {
-    int sockfd, newsockfd, port;            
-    socklen_t clilen;
-    char buffer[512];
-    struct sockaddr_in serv_addr, cli_addr;  
-    int n;
-    if (argc < 2) 
-    {
-        fprintf(stderr,"ERREUR!!!\n");
-        exit(1);
-    }
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0){
-        perror("erreur");
-        exit(1);
-    }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    port = atoi(argv[1]);
-    serv_addr.sin_family = AF_INET;     
-    serv_addr.sin_addr.s_addr = INADDR_ANY; 
-    serv_addr.sin_port = htons(port); 
-    if (bind(sockfd, (struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0){
-        perror("erreur");
-        exit(1);
-    }                                
-    listen(sockfd,5);  
-    clilen = sizeof(cli_addr);  
-    newsockfd = accept(sockfd,(struct sockaddr *) &cli_addr,&clilen);
-    if (newsockfd < 0){
-        perror("erreur");
-        exit(1);
-    }
-    FILE *fp;
-    int rpt = 0;
-    fp = fopen("fichier.txt","a");
-    int mot;
-    read(newsockfd, &mot, sizeof(int));
-    while(rpt != mot)
-    {
-        read(newsockfd , buffer , 512); 
-        fprintf(fp , " %s" , buffer);   
-        rpt++;
-    }
-    printf("Le fichier reçu \n");
-    printf("Le doc creer\n");
-    close(newsockfd);
-    close(sockfd);
-    return 0; 
-}
+    //structure donnant les informations sur le serveur et sur le client
+    struct sockaddr_in information_server;
+    struct sockaddr_in information_client;
 
+    int socketID = socket(AF_INET, SOCK_STREAM, 0);
+    int connexion = 0;
+    int pid;
+    int id;
+    char msg[255];
+    char buffer[512];
+
+    id=0;
+    socklen_t len = sizeof(struct sockaddr_in); //déclaration d' une variable du type socklen_t qui contiendra la taille de la structure
+
+    if (socketID == -1)
+    {
+        perror("socket");
+        exit(-1);
+    }
+
+    /*initialisation du protocole, TCP  l'adresse de connection 127.0.0.1 (en local) et du port du serveur (1400)*/
+    memset(&information_server, 0, sizeof(struct sockaddr_in));
+    information_server.sin_port = htons(2500);
+    information_server.sin_family = AF_INET;
+
+    /* création de la connexion*/
+    if ((bind(socketID, (struct sockaddr *) &information_server, sizeof(struct sockaddr))) == -1)
+    {
+        perror("bind");
+        exit(-1);
+    }
+
+    /* le serveur écoute si un client cherche à se connecter*/
+    if ((listen(socketID, 5)) == -1)
+    {
+        perror("listen");
+        exit (-1);
+    }
+    while (1)
+    {
+
+        memset(&information_client, 0, sizeof(struct sockaddr_in));
+        connexion = accept(socketID, (struct sockaddr *) &information_client, &len); //le serveur accepte la connexion
+
+        if (connexion == -1)
+        {
+            perror("accept");
+            exit(-1);
+        }
+        id+=1;
+        /* Create child process */
+        pid = fork();
+
+        if (pid < 0)
+        {
+            perror("ERROR on fork");
+            exit(1);
+        }
+        if (pid == 0)
+        {
+            /* This is the client process */
+            close(socketID);
+            printf ("Connexion acceptée de : client %i\n",id);
+            memset(msg, 0, 255);
+            sprintf(msg,"bienvenu! client %i",id);
+            send(connexion, msg, strlen(msg), 0);
+            FILE *fp;
+            int rpt = 0;
+            fp = fopen("fichier.txt","a");
+            int mot;
+            read(connexion, &mot, sizeof(int));
+            while(rpt != mot)
+            {
+                read(connexion , buffer , 512); 
+                fprintf(fp , " %s" , buffer);   
+                rpt++;
+            }
+            printf("Le fichier reçu \n");
+            printf("Le doc creer\n");
+        }
+        else
+        {
+            close(connexion);
+
+        }
+
+    }
+    return 0;
+
+}
